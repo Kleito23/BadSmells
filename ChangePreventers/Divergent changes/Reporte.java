1eaf8ad224212e4ff45dc6617cdb6959ed6b8b8e
Diego Bedoya <149976251+DiegoBedoya1@users.noreply.github.com>
2024-12-17 22:44:54 -0500

    reporte.java

---
+++ ChangePreventers/Divergent changes/Reporte.java
@@ -0,0 +1,15 @@
+public class Reporte{
+    private String titulo;
+    private String contenido;
+
+public void generar(){
+    contenido = "contenido del reporte";
+}
+  public String formato(){
+    return "Titulo: "+ titulo +"\n Contenido: "+ contenido;
+  }
+
+public String formatear(){
+  return titlo + "\n" + contenido;
+}
+}
