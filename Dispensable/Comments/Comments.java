dfa63984cda86f23b5928e116f4525faa5a64994
Juseperez <152338766+Juseperez@users.noreply.github.com>
2024-12-17 22:06:10 -0500

    Add files via upload

---
+++ Dispensable/Comments/Comments.java
@@ -0,0 +1,12 @@
+public class Area{
+
+	//Calcula el área de un círculo
+	public double calcularC(double radio){
+		return Math.PI * radio * radio;
+	}
+
+	//Calcula el área de un rectángulo
+	public double calcularR(double largo, double ancho){
+		return largo*ancho;
+	}
+}
